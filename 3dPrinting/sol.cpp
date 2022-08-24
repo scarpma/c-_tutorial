ca44d204434db0269ead0351772d89edefc7dbe4
Martino Andrea Scarpolini <scarpma@Martinos-MacBook-Air.local>
2022-08-25 00:40:38 +0200

    minor

--- 3dPrinting/sol.cpp
+++ 3dPrinting/sol.cpp
@@ -2,19 +2,13 @@
 
 using namespace std;
 
-int sum(int a[])
+int sum(int a[], int N)
 {
-    int N = size(a);
-    //int N = 4;
-    cout << "size a " << sizeof(a) << endl;
-    cout << "size a[0] " << sizeof(a[0]) << endl;
-    cout << "N: " << N << endl;
     int res = 0;
     for (int i=0;i<N;i++)
     {
         res += a[i];
     }
-    cout << res << endl;
     return res;
 }
 
@@ -52,41 +46,42 @@ int max(int a, int b)
 
 int main(void)
 {
-    //int N;
-    //cin >> N;
-
     int s1[4], s2[4], s3[4];
-    cin >> s1[0] >> s1[1] >> s1[2] >> s1[3];
-    cin >> s2[0] >> s2[1] >> s2[2] >> s2[3];
-    cin >> s3[0] >> s3[1] >> s3[2] >> s3[3];
-
+    int T;
     int mins[4];
 
-    for (int i=0;i<4;i++)
-    {
-        mins[i] = min(s1[i], s2[i], s3[i]);
-        cout << mins[i] << endl;
-    }
+    cin >> T;
 
-    if (sum(mins)<1000000)
-    {
-        cout << "IMPOSSIBLE" << endl;
-    } else
-    {
-        int sol[] = {0,0,0,0};
-        int i = 0;
-        while (sum(sol) < 1000000)
+    for (int kkk=0;kkk<T;kkk++) {
+
+        cout << "case #" << kkk + 1 << ": ";
+
+        cin >> s1[0] >> s1[1] >> s1[2] >> s1[3];
+        cin >> s2[0] >> s2[1] >> s2[2] >> s2[3];
+        cin >> s3[0] >> s3[1] >> s3[2] >> s3[3];
+
+
+        for (int i=0;i<4;i++) // find minimum fill for each color between the printers
         {
-            sol[i] = min(mins[i], 1000000);
-            i++;
-            if (sum(sol) > 1000000)
-            {
-                sol[i] -= sum(sol) - 1000000;
-            }
+            mins[i] = min(s1[i], s2[i], s3[i]);
         }
-        cout << sol[0] << " " << sol[1] << " " << sol[2] << " " << sol[3] << endl;
+
+        if (sum(mins, 4)<1000000)
+        {
+            cout << "IMPOSSIBLE" << endl;
+        } else
+        {
+            int sol[] = {0,0,0,0};
+            int k = 0;
+            while (sum(sol, 4) < 1000000)
+            {
+                sol[k] = min(mins[k], 1000000 - sum(sol, 4));
+                k++;
+            }
+            //cout << "case #" << 
+            cout << sol[0] << " " << sol[1] << " " << sol[2] << " " << sol[3] << endl;
+        }
     }
-    //cout << mins[0] << " " << mins[1] << " " << mins[2] << " " << mins[3] << endl;
 
     return 0;
 }
