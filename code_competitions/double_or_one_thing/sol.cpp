7aab0968e33c90f1c44491c0836556b2d3915e47
scarpma <scarpma@gmail.com>
2022-08-25 18:43:00 +0200

    minor

---
+++ code_competitions/double_or_one_thing/sol.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int getint (char c) {
+    int i = c;
+    return i;
+}
+
+int main(void) {
+    int T;
+    string tr_str;
+
+    cin >> T;
+
+    for (int i=0;i<T;i++) {
+        
+        string str;
+        cin >> str;
+        
+
+
+        cout << "case #" << i << ": " << tr_str << endl;
+    }
+
+    return 0;
+}
