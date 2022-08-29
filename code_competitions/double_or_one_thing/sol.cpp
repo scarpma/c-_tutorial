8fa4dc3b8a3d470ca4ba5291dcbb09149020fb2d
Martino Andrea Scarpolini <scarpma@Martinos-MacBook-Air.local>
2022-08-30 01:24:32 +0200

    minor

--- code_competitions/double_or_one_thing/sol.cpp
+++ code_competitions/double_or_one_thing/sol.cpp
@@ -7,20 +7,54 @@ int getint (char c) {
     return i;
 }
 
+char next_diff_char(const string str, const int i) {
+    int j = i+1;
+    for (int j=i+1;j<str.size();j++) {
+        if (str[j] != str[i]) {
+            return str[j];
+        }
+    }
+    char res = '@';
+    return res;
+}
+
+string transform_string(string str) {
+    int i=0;
+    string tr_string;
+    char next;
+
+    tr_string.reserve(2*str.size());
+
+    // add new char after char[j] if it is convenient
+    while (i<str.size()-1) {
+        tr_string += str[i];
+        next = next_diff_char(str,i);
+        if (next == '@') {i++; break;}
+        if (getint(str[i]) <= getint(next)) {
+            tr_string += str[i];
+        }
+        i++;
+    }
+    // fill remaining chars
+    while (i<str.size()) {
+        tr_string += str[i];
+        i++;
+    }
+
+    return tr_string;
+}
+
 int main(void) {
     int T;
-    string tr_str;
+    string str, tr_str;
 
     cin >> T;
 
     for (int i=0;i<T;i++) {
-        
         string str;
         cin >> str;
-        
-
-
-        cout << "case #" << i << ": " << tr_str << endl;
+        tr_str = transform_string(str);
+        cout << "Case #" << i+1 << ": " << tr_str << endl;
     }
 
     return 0;
